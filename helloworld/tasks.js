a66d425a26ece24c02202b6d9b3a77dd7ec9c2d0
hidega <hidega@caesar.elte.hu>
2020-09-13 21:33:13 +0200

    dev

--- helloworld/tasks.js
+++
@@ -1,6 +0,0 @@
-'use strict'
-
-const BuildTasks = require('@permian/build-tasks')
-const pkg = require('./package.json')
-
-module.exports = new BuildTasks({ pkg, transpile: { compact: true, minify: true }})
