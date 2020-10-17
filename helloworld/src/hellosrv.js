cbbf6a38958b6cf8d9a7f3e4671ccd58947f89c9
hidega <hidega@caesar.elte.hu>
2020-10-17 11:47:03 +0200

    dev

--- helloworld/src/hellosrv.js
+++ helloworld/src/hellosrv.js
@@ -3,9 +3,18 @@
 var http = require('http')
 
 module.exports = cfg => {
+  var id = 0
+
+  var message = () => `<?xml version="1.0" encoding="UTF-8"?>
+<message>
+  <text>Hello world!</text>
+  <id>${id = ++id % Number.MAX_SAFE_INTEGER}</id>
+  <timestamp>${new Date().toISOString()}</timestamp>
+</message>`
+
   var server = http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'application/xml' })
-    res.write(`<?xml version="1.0" encoding="UTF-8"?><p>Hello world!<br/>${new Date().toISOString()}</p>`)
+    res.write(message())
     res.end()
   })
   cfg.socket ? server.listen(cfg.socket) : server.listen(cfg.port, cfg.host)
