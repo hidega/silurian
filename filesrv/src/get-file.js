b2bd10fdbe7ca154668bb859052d37bfe1121da1
hidega <hidega@caesar.elte.hu>
2020-11-01 10:13:57 +0100

    dev

--- filesrv/src/get-file.js
+++ filesrv/src/get-file.js
@@ -4,8 +4,6 @@ var commons = require('./commons')
 var mimeTypes = require('./ext-mtype')
 var Handler = require('./handler')
 
-var chunkSize = 1000 * 256
-
 function GetFileHandler(context, ioaFactory, params) {
   Handler.call(this, context, ioaFactory, params)
 
@@ -18,33 +16,25 @@ function GetFileHandler(context, ioaFactory, params) {
     writer.close()
   }
 
-  var readFile = (fd, contentType) => {
-    var writerError = 0
+  var streamFile = (filePath, contentType) => {
+    var writeError = writer => {
+      writer.write(Buffer.from('-------FILE_READ_ERROR-------'))
+      writer.close()
+    }
     var gzipped = this.isGzipped(context.getRequestParameters())
     var writer = ioaFactory.createBinaryResponseWriter({
       gzipped,
       contentType: gzipped ? 'application/gzip' : contentType,
       httpResposeCode: 200
-    }, event => writerError |= Number(event === 'error'))
-    var f = () => commons.fs.read(fd, Buffer.alloc(chunkSize), 0, chunkSize, null, (err, bytesRead, buf) => commons.matcher()
-      .on(!writerError && err, () => {
-        writer.write(Buffer.from('-------FILE_READ_ERROR-------'))
-        writer.close()
-      })
-      .on(!writerError && bytesRead > 0, () => {
-        writer.write(buf.subarray(0, bytesRead))
-        setImmediate(f)
-      })
-      .otherwise(() => writer.close()))
-    f()
+    }, event => event === 'error' && writeError(writer))
+    var readStream = commons.fs.createReadStream(filePath, {}).on('error', () => writeError(writer))
+    readStream.pipe(writer.getOutputStream())
   }
-
-  var openFile = (filePath, contentType) => commons.fs.open(filePath, 'r', (err, fd) => err ? error(404) : readFile(fd, contentType))
 
   this.handle = () => commons.try(() => {
     var filePath = this.resolvePath(context.getRequestParameters().path || context.getRemainingPath().slice(1).join('/'))
     var contentType = Object.assign(mimeTypes, params.additionalTypeMappings)[this.getFileExtension(filePath)] || 'application/octet-stream'
-    commons.fs.lstat(filePath, (err, stats) => err || !stats.isFile() ? error(404) : openFile(filePath, contentType))
+    commons.fs.lstat(filePath, (err, stats) => err || !stats.isFile() ? error(404) : streamFile(filePath, contentType))
   }, () => error(500))
 }
 
