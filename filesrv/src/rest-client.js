4f00841c51d9688482f52b25b48d3aa691034ee0
hidega <hidega@caesar.elte.hu>
2020-11-15 11:50:10 +0100

    dev

--- filesrv/src/rest-client.js
+++ filesrv/src/rest-client.js
@@ -1,6 +1,6 @@
 'use strict'
 
-var axios = require('axios')
+var restclient = require('@permian/restclient')
 
 function RestClient(opts) {
   var options = Object.assign({
@@ -8,21 +8,21 @@ function RestClient(opts) {
     url: 'http://127.0.0.1:5802/file-service'
   }, opts)
 
-  var axiosGet = p => axios({
+  var httpGet = p => restclient({
     method: 'get',
     timeout: p.timeoutMs || options.defaultTimeoutMs,
     responseType: p.responseType || 'json',
     url: p.url
   })
 
-  this.getFile = (filename, p, params = (p || {})) => axiosGet({
+  this.getFile = (filename, p, params = (p || {})) => httpGet({
     responseType: params.stream ? 'stream' : 'arraybuffer',
     url: options.url + '/get-file?path=' + filename.replace(/^\/+/, '') + (params.zipped ? '&zipped=1' : '')
   })
 
-  this.listDirectory = (dirname, timeoutMs) => axiosGet({ url: options.url + '/list-directory?path=' + dirname, timeoutMs })
+  this.listDirectory = (dirname, timeoutMs) => httpGet({ url: options.url + '/list-directory?path=' + dirname, timeoutMs })
 
-  this.ping = timeoutMs => axiosGet({ url: options.url + '/ping', timeoutMs })
+  this.ping = timeoutMs => httpGet({ url: options.url + '/ping', timeoutMs })
 }
 
 module.exports = RestClient
